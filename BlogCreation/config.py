ba767ad35d8796fc289971f061b11a43c148e94c
Ifyanyit <ngeneifyanyi@gamail.com>
2023-01-05 15:09:19 +0100

    Blog app

---
+++ BlogCreation/config.py
@@ -0,0 +1,19 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    #Send error via mails 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['ngeneifyanyi@gmail.com'] # list of emails that will receive error reports
+    
+    # Pagination 
+    POSTS_PER_PAGE = 25 # a configuration item to the application that determines how many items will be displayed per page.
